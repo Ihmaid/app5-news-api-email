7773f67a20113038fb45538520cb81163b268ea7
Ihmaid <gabriel-ihmaid@ufrrj.br>
2024-06-05 09:52:18 -0300

    Refined the program, add links, subject, fixed size and topic, day 28

--- send_email.py
+++ send_email.py
@@ -4,16 +4,23 @@ import os
 
 
 def send_email(message):
+    # Native configurations for Gmail
     host = "smtp.gmail.com"
     port = 465
 
+    # This email is the sender of the message
     username = "teste1111python@gmail.com"
-    # Environmental variable
+    # Environmental variable that refers to the app password
     password = os.getenv("PASSWORD")
 
+    # This email is the receiver of the message
     receiver = "gabrielihm.ave@gmail.com"
+    # Creation of a SSL context
     context = ssl.create_default_context()
 
+    # Creation of a server
     with smtplib.SMTP_SSL(host, port, context=context) as server:
+        # The login with the email and the respective app password
         server.login(username, password)
+        # Function to send the email
         server.sendmail(username, receiver, msg=message)
